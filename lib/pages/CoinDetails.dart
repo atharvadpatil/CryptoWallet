d9d9ee6c4931420d8c3e0f7a4a06d90fa48a6d1e
Atharva Patil <atharva.dp@somaiya.edu>
2021-11-25 13:55:36 +0530

    details shown on Coin details page

--- lib/pages/CoinDetails.dart
+++ lib/pages/CoinDetails.dart
@@ -31,6 +31,55 @@ class CoinDetails extends StatefulWidget {
 }
 
 class _CoinDetailsState extends State<CoinDetails> {
+  TextEditingController _textFieldController = TextEditingController();
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add TO Wallet'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Enter Amount"),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: Text('ADD'),
+                onPressed: () {
+                  setState(() {
+                    codeDialog = valueText;
+                    setState(() {
+                      valueText = "";
+                    });
+                    Navigator.pop(context);
+                    print(codeDialog);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  String codeDialog="";
+  String valueText="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +87,220 @@ class _CoinDetailsState extends State<CoinDetails> {
         title: Text('CryptoWallet'),
         centerTitle: true,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
         child: Column(
-          children: [
-            Text(widget.name),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 60.0,
+                backgroundImage: NetworkImage('https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'),
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Symbol:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'btc',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Name:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'Bitcoin',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Current Price:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '4000000',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Market Cap:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '200000000',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Market Cap Rank:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  '24h High:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '4100000',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  '24h Low:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '3900000',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  'Price Change in 24h:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '100000',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  '% Price change in 24h:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '1%',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 40.0),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                ),
+                onPressed: () {
+                  _displayTextInputDialog(context);
+                },
+                child: Text("Add To Wallet".toUpperCase(), style: TextStyle(fontSize: 14)),
+              ),
+            ),
           ],
         ),
       ),
