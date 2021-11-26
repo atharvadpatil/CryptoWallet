324eeb6572c2afe90a5c5d8355816a4d9bd55631
Atharva Patil <atharva.dp@somaiya.edu>
2021-11-26 13:21:23 +0530

    add and remove from watchlist function done

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -1,5 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cryptowallet/main.dart';
+import 'package:cryptowallet/pages/WatchList.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../globals.dart' as globals;
 
 Future<bool> signIn(String email, String password) async {
   try {
@@ -33,9 +37,7 @@ Future<bool> register(String email, String password) async {
 Future<void> signOut() async
 {
   try{
-
     await FirebaseAuth.instance.signOut();
-
   }
   catch(e)
   {
@@ -43,4 +45,74 @@ Future<void> signOut() async
   }
 }
 
+Future<void> addToWatchList(String id, BuildContext context) async{
+  String uid = FirebaseAuth.instance.currentUser.uid;
+  DocumentReference documentReference = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(uid);
+  FirebaseFirestore.instance.runTransaction((transaction) async {
+    DocumentSnapshot snapshot = await transaction.get(documentReference);
+    if (!snapshot.exists) {
+      documentReference.set({'Watchlist': [id]});
+    }
+    else {
+      List<dynamic> newWatchlist = snapshot.data()['Watchlist'];
+      if(newWatchlist.contains(id)){
+        newWatchlist=newWatchlist;
+        print("Already present in watchlist");
+      }
+      else{
+        newWatchlist.add(id);
+      }
+      transaction.update(documentReference, {'Watchlist': newWatchlist});
+    }
+  })
+  .then((value) {
+    globals.selectedIndex=1;
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+    );
+  })
+  .catchError((e) {
+    print("Coin is not added to watchlist");
+  });
+}
 
+
+// Future<List<dynamic>> readCurrentUserWatchlist() async{
+//   String uid = FirebaseAuth.instance.currentUser.uid;
+//   DocumentReference documentReference = FirebaseFirestore.instance
+//       .collection('Users')
+//       .doc(uid);
+//   List<dynamic> watchlist=[];
+//   FirebaseFirestore.instance.runTransaction((transaction) async {
+//     DocumentSnapshot snapshot = await transaction.get(documentReference);
+//     if (snapshot.exists) {
+//       watchlist = snapshot.data()['Watchlist'];
+//       print(watchlist);
+//       return watchlist;
+//     }
+//   });
+//   return watchlist;
+// }
+
+Future<void> removeFromWatchList(String id, BuildContext context) async{
+  String uid = FirebaseAuth.instance.currentUser.uid;
+  DocumentReference documentReference = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(uid);
+  FirebaseFirestore.instance.runTransaction((transaction) async {
+    DocumentSnapshot snapshot = await transaction.get(documentReference);
+    List<dynamic> newWatchlist = snapshot.data()['Watchlist'];
+    bool removed = newWatchlist.remove(id);
+    transaction.update(documentReference, {'Watchlist': newWatchlist});
+  }).then((value) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+    );
+  }).catchError((e) {
+    print("Error");
+  });
+}
